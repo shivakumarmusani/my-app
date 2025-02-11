737c6b32a06dbca8e2e100888978ff28ed1de931
shivakumarmusani <shivakumarmusani@gmail.com>
2025-02-11 23:31:47 +0530

    add

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
+import RestaurantList from "./RestaurantList";
 
 function Home(){
     return(
         <div>
         <h1>welcome home dhjkfhaksdfjkfjkasbd</h1>
+        <RestaurantList/>
+
         </div>
     );
 }
