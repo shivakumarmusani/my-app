3de6ee93ef4252c3e56294f8e4ead3f4da6a7eac
shivakumarmusani <shivakumarmusani@gmail.com>
2025-02-11 20:40:51 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import logo from './logo.svg';
+//import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./Components/Pages/Home"; //Home page
+import RestaurantList from "./Components/Pages/RestaurantList";// Restaurant List
+//import RestaurantDetail from "./Components/Pages/RestaurantDetail";//Individual Restaurant Detail
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/RestaurantList" element={<RestaurantList />} />
+      </Routes>
+      </BrowserRouter>
   );
 }
-
 export default App;
